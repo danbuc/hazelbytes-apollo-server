3ba46f4ee3c35ac6a9d50cd97ea9b2876ad7a676
Daniel Buchbauer <daniel@just-my-work.de>
2018-12-16 19:25:47 +0100

    feat: add models and typedefs

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose')
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+
 
 require('dotenv').config({ path: 'variables.env' })
+
+const User = require('./models/User')
+const Post = require('./models/Post')
 
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log('Database connected'))
     .catch(err => console.error(err));
 
-const typeDefs = gql `
-    type Query {
-        getMangaSeries: [MangaSeries]
-    }
-
-    type MangaSeries {
-        title: String
-        currentAvailable: String
-        currentRead: String
-        completedByAuthor: Boolean
-    }
-`;
 
 const server = new ApolloServer({
-    typeDefs
+    typeDefs,
+    context: {
+        User,
+        Post
+    }
 });
 
 server.listen().then(({ url }) => {
